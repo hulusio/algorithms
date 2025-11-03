4b51d897417168625e1234708f46316b4e0b2c04
hulusio <hulusi144@gmail.com>
2025-11-03 11:56:28 +0300

    Update bubble_sort.py
    Signed-off-by: hulusio <hulusi144@gmail.com>

--- 01-Sorting/1-Bubble Sort/bubble_sort.py
+++ 01-Sorting/1-Bubble Sort/bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble_sort(lst):
   length_list = len(lst)
   for i in range(length_list - 1):
-  	// In each pass, the largest elements are pushed to the end
+  	# In each pass, the largest elements are pushed to the end
     for j in range(length_list - 1 - i):
       if lst[j] > lst[j+1]:
         temp = lst[j]
@@ -10,6 +10,6 @@ def bubble_sort(lst):
             
 if __name__ == "__main__":
 	lst = [5, 3, 8, 4, 2, 1, 7, 5, 0]
-    //sort function
+    # sort function
 	bubble_sort(lst)
 	print(lst);
