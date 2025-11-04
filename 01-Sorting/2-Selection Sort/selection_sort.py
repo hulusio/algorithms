794124225a5ac0610fd95ee284ca72316cd9e63a
hulusio <hulusi144@gmail.com>
2025-11-04 22:14:30 +0300

    Refactor selection sort implementation
    Refactor selection sort to use min_val and min_index for finding the smallest element.
    
    Signed-off-by: hulusio <hulusi144@gmail.com>

--- 01-Sorting/2-Selection Sort/selection_sort.py
+++ 01-Sorting/2-Selection Sort/selection_sort.py
@@ -1,20 +1,19 @@
 def selection_sort(lst):
-  smallest = 100;
-  length_list = len(lst)
-  for i in range(length_list):
-    smallest = 100
+    length_list = len(lst)
+    for i in range(length_list):
+        min_val   = lst[i]
+        min_index = i
   	# In each pass, the largest elements are pushed to the end
-    for j  in range(i, length_list):
-      if lst[j] < smallest:
-        smallest       = lst[j]
-        smallest_index = j
-    #print(smallest)
-    temp = lst[i]
-    lst[i]  = smallest
-    lst[smallest_index] = temp
+        for j  in range(i + 1, length_list):
+            if lst[j] < min_val:
+                min_val       = lst[j]
+                min_index = j
+    			#print(smallest)
+        lst[i], lst[min_index] = lst[min_index], lst[i]
              
 if __name__ == "__main__":
-	lst = [5, 3, 8, 1, 4, 2, 0]
+	lst = [5, -6, 8, 1, 4, 2, 0]
     # sort function
 	selection_sort(lst)
 	print(lst);
+
