756029c90552444005770f78c0fb0b6a40dd97e4
hulusio <hulusi144@gmail.com>
2025-11-12 15:52:31 +0300

    Update video3- bağlı liste araya eleman ekleme ve araya eleman ekleme ds.c
    Signed-off-by: hulusio <hulusi144@gmail.com>

--- DSA/ds3/ds.c
+++ DSA/ds3/ds.c
@@ -30,64 +30,49 @@ void ekle(node *r, int val)
     iter->next = NULL;
 }
 
-void ekle_sirali(node *r, int val)
+node * ekle_sirali(node * r, int val)
 {
     if(r == NULL) 
     {
         r = (node*)malloc(sizeof(node)); 
         r->next = NULL;
         r->x = val;        
-        return;
+        return r;
     }
-
-    while(r->next != NULL && r->next->x < val)
+    if (r->x > val)
     {
-        r = r->next;
+        node *yeni = (node*)malloc(sizeof(node));
+    	yeni->x = val;
+    	yeni-> next = r;
+    	return yeni;    
+	}
+    node * iter;
+    iter = r;
+    while(iter->next != NULL && iter->next->x < val)
+    {
+        iter = iter->next;
     }
     node *yeni = (node*)malloc(sizeof(node));
     yeni->x = val;
-    yeni-> next = r->next;
+    yeni-> next = iter->next;
+    iter->next = yeni;
+    return r;
+
+
 
 }
 int main() 
 {
-  printf("Hello World ds3! \n");
-  node *root = (node*)malloc(sizeof(node)); 
-  root->x = 88;
-  
-  node * iter = root;
-  for(int i =0; i<10; i++)
-  {
-    iter->next = (node*)malloc(sizeof(node));
-    iter = iter->next; 
-    iter->x = 10 * i;      
-    iter->next = NULL;
-  }
-  bastir(root);
-  ekle(root, 999);
+  	printf("Hello World ds3! \n");
+	node * root;
+    root = NULL;
+    root = ekle_sirali(root, 400); 
+    root = ekle_sirali(root, 50);
+    root = ekle_sirali(root, 40);
+    root = ekle_sirali(root, 550);   
+    root = ekle_sirali(root, 450); 
 
-  printf("----\n");
-
-  node *araya = (node*)malloc(sizeof(node)); 
-  araya->x = 777;
-  iter = root;
-  for (size_t i = 0; i < 3; i++)
-  {
-    iter = iter->next;
-    /* code */
-  }
-   araya->next = iter->next;
-   iter->next = araya;
-   bastir(root);
-
-    printf("---- sirali ekleme\n");
-    node *root2 ;
-    root2 = NULL;
-    ekle_sirali(root2, 50);
-    printf("root2.x: %d\n", root2->x);
-    //ekle_sirali(root2, 30);
-    //ekle_sirali(root2, 40);
-    bastir(root2);
+    bastir(root);
   return 0; 
 
 }
