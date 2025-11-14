9a5ec8fe4617d2a86037314438602e0b5c07b511
hulusio <hulusi144@gmail.com>
2025-11-14 17:24:09 +0300

    Create ds4 day ! ds.c
    Signed-off-by: hulusio <hulusi144@gmail.com>

---
+++ DSA/ds4/ds.c
@@ -0,0 +1,79 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct n{
+	int x;
+    struct n *next;    
+}n;
+typedef n node;
+
+void bastir( node *r)
+{
+    node *iter = r;
+    while(iter != NULL)
+    {
+        printf("%4d  ", iter->x);
+        iter = iter->next;
+    }
+    printf("\n");
+}
+void ekle(node *r, int val)
+{
+    node *iter = r;
+    while(iter->next != NULL)
+    {
+        iter = iter->next;
+    }
+    iter->next = (node*)malloc(sizeof(node));
+    iter = iter->next; 
+    iter->x = val;      
+    iter->next = NULL;
+}
+
+node * ekle_sirali(node * r, int val)
+{
+    if(r == NULL) 
+    {
+        r = (node*)malloc(sizeof(node)); 
+        r->next = NULL;
+        r->x = val;        
+        return r;
+    }
+    if (r->x > val)
+    {
+        node *yeni = (node*)malloc(sizeof(node));
+    	yeni->x = val;
+    	yeni-> next = r;
+    	return yeni;    
+	}
+    node * iter;
+    iter = r;
+    while(iter->next != NULL && iter->next->x < val)
+    {
+        iter = iter->next;
+    }
+    node *yeni = (node*)malloc(sizeof(node));
+    yeni->x = val;
+    yeni-> next = iter->next;
+    iter->next = yeni;
+    return r;
+
+
+
+}
+int main() 
+{
+  	printf("Hello World ds4: Listeden eleman silme! \n");
+	node * root;
+    root = NULL;
+    root = ekle_sirali(root, 400); 
+    root = ekle_sirali(root, 50);
+    root = ekle_sirali(root, 40);
+    root = ekle_sirali(root, 550);   
+    root = ekle_sirali(root, 450); 
+
+    bastir(root);
+  return 0; 
+
+}
